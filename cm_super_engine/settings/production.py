cf822aa80bd0ccff75b2a364e26b4406e7f6a64f
Aaron Dominguez <aarondominguez@Aarons-MacBook-Pro.local>
2020-08-27 00:13:08 +0200

    Create settings and requirements file

---
+++ cm_super_engine/settings/production.py
@@ -0,0 +1,7 @@
+import os
+
+from .base import *
+
+DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
--- cm_super_engine/wsgi.py
+++ cm_super_engine/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cm_super_engine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cm_super_engine.settings.local')
 
 application = get_wsgi_application()
