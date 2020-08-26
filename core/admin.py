c5bbab7a4668eb12d03169ae3494f29c91d67101
Aaron Dominguez <aronpes94@gmail.com>
2020-08-27 01:08:52 +0200

    Create models for bouth CM and Businesss

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from core.models import Business, CommunityManager
 
-# Register your models here.
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = (
+        'business_name',
+        'business_email',
+        'business_address'
+    )
+
+
+class CommunityManagerAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number'
+    )
+
+admin.site.register(Business, BusinessAdmin)
+admin.site.register(CommunityManager, CommunityManagerAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Business(models.Model):
+    manager_first_name = models.CharField(max_length=50)
+    manager_last_name = models.CharField(max_length=50)
+    manager_email = models.EmailField(max_length=50)
+    # TODO check phone number field for validation
+    manager_phone_number = models.CharField(max_length=20)
+    business_name = models.CharField(max_length=50)
+    business_email = models.EmailField(max_length=50)
+    business_country = models.CharField(max_length=50)
+    business_city = models.CharField(max_length=50)
+    business_address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.business_name}'
+
+
+
+class CommunityManager(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    # TODO check phone number field for validation
+    phone_number = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name}'
