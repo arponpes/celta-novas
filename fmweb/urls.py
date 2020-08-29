289d2e42dc4b62e0924fece4738e15452dcb2db2
Aaron Dominguez <aronpes94@gmail.com>
2020-08-29 20:43:01 +0200

    Create player form

--- fmweb/urls.py
+++ fmweb/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.LandingPageView.as_view(), name='landing_page'),
+    path('create_player', views.PlayerCreate.as_view(), name='player_form'),
 ]
--- fmweb/views.py
+++ fmweb/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 
-from .models import Player
+from .models import Player, POSITIONS, OPINION
+
 
 class LandingPageView(TemplateView):
 
-    template_name = "landing_page.html"
+    template_name = "fmweb/landing_page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['players'] = Player.objects.all()
+        return context
+
+class PlayerCreate(CreateView):
+    model = Player
+    fields = [
+        'name',
+        'position',
+        'opinion',
+        'notes'
+    ]
+
+    success_url = reverse_lazy('landing_page')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['positions'] = POSITIONS
+        context['opinions'] = OPINION
         return context
