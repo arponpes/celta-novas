f7253ec4095f8c3a2c41f04aea2b9475ce294878
Aaron Dominguez <aronpes94@gmail.com>
2021-06-26 21:59:13 +0200

    Add tox

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ## Getting Started
 
+### Production
+
+docker-compose -f docker-compose.prod.yml up --detach --build
+
+### Local
+
 docker-compose up --build
+
+### Tests
+
+pip install tox
+tox
--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -2,11 +2,11 @@ import time
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management import BaseCommand
- 
- 
+
+
 class Command(BaseCommand):
     """Django command to pause execution until db is available"""
- 
+
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_conn = None
@@ -16,5 +16,5 @@ class Command(BaseCommand):
             except OperationalError:
                 self.stdout.write('Database unavailable, waititng 1 second...')
                 time.sleep(1)
- 
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
