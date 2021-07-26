1955fc44ea8d4f7982d98fe75024ff2a80113ef4
Aaron Dominguez <aronpes94@gmail.com>
2021-07-26 14:32:55 +0200

    Add checks after task is executed

--- core/tasks.py
+++ core/tasks.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from celery import shared_task
 from core.scrapers.faro_de_vigo_scraper import execute_fv_scraper
 from core.scrapers.la_voz_scraper import execute_lv_scraper
@@ -8,18 +10,30 @@ from core.scrapers.moi_celeste_scraper import execute_mc_scraper
 @shared_task
 def task_fv_scraper():
     execute_fv_scraper()
+    requests.get(
+        f'https://hc-ping.com/{os.environ.get("fv_hc_ping")}', timeout=10
+    )
 
 
 @shared_task
 def task_lv_scraper():
     execute_lv_scraper()
+    requests.get(
+        f'https://hc-ping.com/{os.environ.get("lv_hc_ping")}', timeout=10
+    )
 
 
 @shared_task
 def task_marca_scraper():
     execute_marca_scraper()
+    requests.get(
+        f'https://hc-ping.com/{os.environ.get("marca_hc_ping")}', timeout=10
+    )
 
 
 @shared_task
 def task_mc_scraper():
     execute_mc_scraper()
+    requests.get(
+        f'https://hc-ping.com/{os.environ.get("mc_hc_ping")}', timeout=10
+    )
