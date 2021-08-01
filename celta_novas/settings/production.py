e874934e4670ce810c9c2ce719d99e3674daa54a
Aaron Dominguez <aronpes94@gmail.com>
2021-08-01 18:45:56 +0200

    Move redis to unly use it in production

--- celta_novas/settings/production.py
+++ celta_novas/settings/production.py
@@ -2,3 +2,15 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
+}
