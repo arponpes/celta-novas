f6b2bc05b139060c0230ae0e0381a2a2b2b84e6f
Aaron Dominguez <aronpes94@gmail.com>
2021-08-20 18:15:34 +0200

    Fix FV news from all sports

--- core/scrapers/faro_de_vigo_scraper.py
+++ core/scrapers/faro_de_vigo_scraper.py
@@ -9,7 +9,14 @@ URL = f'{URL_PREFIX}celta-de-vigo/'
 
 def get_articles() -> list:
     soup = get_soup(URL)
-    return soup.find_all('a', class_='new__headline')
+    articles = soup.find_all('article', class_='new')
+    celta_articles = []
+    for article in articles:
+        if article.select('a[href="/celta-de-vigo/"]'):
+            celta_articles.append(
+                article.find_all(class_='new__headline')[0]
+            )
+    return celta_articles
 
 
 def update_articles(articles):
