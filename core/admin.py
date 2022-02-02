94f71385b25f215656789d71cfd8f66c6414ad67
Aaron Dominguez <aronpes94@gmail.com>
2022-02-02 19:57:40 +0100

    Customize admin panel

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from core.models import Article
 
-admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at", "source")
+    list_filter = ("source",)
