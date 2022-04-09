ea9d951bde8aec3aa41474b65ceef5a08390f0ec
Aaron Dominguez <aronpes94@gmail.com>
2022-04-09 19:51:55 +0200

    Create second chart

--- core/helpers/article_metrics_generator.py
+++ core/helpers/article_metrics_generator.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, date
 
 from core import models
 from django.db.models import Count
@@ -18,7 +18,12 @@ class ArticleMetricsGenerator:
         articles_metrics["source_with_more_articles"] = self.get_source_with_more_articles()
 
         articles_metrics["source_with_more_articles_last_24_hours"] = self.get_source_with_more_articles_last_24_hours()
+
         articles_metrics["articles_by_source"] = self.get_articles_by_source()
+
+        from_date, to_date = self.last_week_date_range()
+        articles_metrics["articles_last_week_by_date"] = self.get_articles_by_date(from_date, to_date)
+        articles_metrics["articles_last_week_by_date_by_source"] = self.get_articles_by_date_by_source(from_date)
 
         return articles_metrics
 
@@ -50,3 +55,30 @@ class ArticleMetricsGenerator:
         for source in models.Article.SOURCE_CHOICES:
             articles_by_source[source[0]] = self.articles.filter(source=source[0]).count()
         return articles_by_source
+
+    def get_articles_by_date(self, start_date, end_date) -> dict:
+        return (
+            self.articles.filter(created_at__gte=start_date)
+            .values("created_at__date")
+            .annotate(count=Count("id"))
+            .values("created_at__date", "count")
+            .order_by("created_at__date")
+        )
+
+    def get_articles_by_date_by_source(self, start_date) -> dict:
+        articles_by_date_by_source = {}
+        for source in models.Article.SOURCE_CHOICES:
+            articles_by_date_by_source[source[0]] = (
+                self.articles.filter(created_at__gte=start_date, source=source[0])
+                .values("created_at__date")
+                .annotate(count=Count("id"))
+                .values("created_at__date", "count")
+                .order_by("created_at__date")
+            )
+        return articles_by_date_by_source
+
+    def last_week_date_range(self) -> tuple:
+        today = date.today()
+        start_date = today - timedelta(days=7)
+        end_date = today
+        return start_date, end_date
--- web/static/js/stats.js
+++ web/static/js/stats.js
@@ -1,18 +1,25 @@
+const colors = {
+    'TOTAL': 'rgb(54, 162, 235)',
+    'MARCA': 'rgb(255, 107, 107)',
+    'MOI CELESTE': 'rgb(255, 217, 61)',
+    'FARO DE VIGO': 'rgb(107, 203, 119)',
+    'LA VOZ DE GALICIA': 'rgb(77, 150, 255)',
+}
+
+
 async function fetchData() {
     let response = await fetch('/api/articles_metrics');
 
-    console.log(response.status); // 200
-    console.log(response.statusText); // OK
-
     if (response.status === 200) {
         let data = await response.json();
-        renderChart(data);
+        renderChartArticlesBySource(data);
+        renderChartArticlesByWeekDay(data)
     }
 }
 
 
-let renderChart = function (data) {
-    const ctx = document.getElementById("chart").getContext("2d");
+let renderChartArticlesBySource = function (data) {
+    const ctx = document.getElementById("chart_articles_by_source").getContext("2d");
     let labels = []
     let data_values = []
     for (let source in data['articles_by_source']) {
@@ -25,19 +32,19 @@ let renderChart = function (data) {
         data: {
             labels: labels,
             datasets: [{
-                label: 'Numero de artigos',
+                label: 'Total',
                 data: data_values,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
+                    colors['MARCA'],
+                    colors['MOI CELESTE'],
+                    colors['FARO DE VIGO'],
+                    colors['LA VOZ DE GALICIA'],
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
                 ],
                 borderWidth: 1
             }]
@@ -47,9 +54,64 @@ let renderChart = function (data) {
                 y: {
                     beginAtZero: true
                 }
-            }
+            },
         }
     });
 }
+
+
+let renderChartArticlesByWeekDay = function (data) {
+    const ctx = document.getElementById("chart_articles_by_week_day").getContext("2d");
+    let labels = []
+    let dataValuesTotal = []
+
+    for (date of data['articles_last_week_by_date']) {
+        labels.push(date['created_at__date']);
+        dataValuesTotal.push(date['count']);
+    }
+    let dataSets = [{
+        label: 'Total',
+        data: dataValuesTotal,
+        borderColor: colors['TOTAL'],
+        borderWidth: 1
+    }]
+
+    for (const [source, dataData] of Object.entries(data['articles_last_week_by_date_by_source'])) {
+        let sourceData = []
+        for (date of dataData) {
+            sourceData.push(date['count'])
+        }
+        let dataSet = {
+            label: source,
+            data: sourceData,
+            borderWidth: 1,
+            borderColor: colors[source]
+        }
+        dataSets.push(dataSet);
+    }
+    console.log(dataSets)
+
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: dataSets
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart'
+                }
+            }
+        },
+    });
+}
+
 
 fetchData();
