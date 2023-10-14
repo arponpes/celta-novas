6e2c49d49c63a9c4c8b37a69be480aefeb93c002
Aaron Dominguez <aaron.dominguez@midokura.com>
2023-10-15 01:26:17 +0200

    Remove hc-ping

--- core/tasks.py
+++ core/tasks.py
@@ -15,31 +15,21 @@ from core.helpers.healthcheck import check_article_status
 def task_fv_scraper():
     ap = ArticleProcessor(FaroDeVigoCrawler)
     ap.process_articles()
-    requests.get(f'https://hc-ping.com/{os.environ.get("fv_hc_ping")}', timeout=10)
 
 
 @shared_task
 def task_lv_scraper():
     ap = ArticleProcessor(LaVozDeGaliciaCrawler)
     ap.process_articles()
-    requests.get(f'https://hc-ping.com/{os.environ.get("lv_hc_ping")}', timeout=10)
 
 
 @shared_task
 def task_marca_scraper():
     ap = ArticleProcessor(MarcaCrawler)
     ap.process_articles()
-    requests.get(f'https://hc-ping.com/{os.environ.get("marca_hc_ping")}', timeout=10)
 
 
 @shared_task
 def task_mc_scraper():
     ap = ArticleProcessor(MoiCelesteCrawler)
     ap.process_articles()
-    requests.get(f'https://hc-ping.com/{os.environ.get("mc_hc_ping")}', timeout=10)
-
-
-@shared_task
-def task_check_article_status():
-    if check_article_status():
-        requests.get(f'https://hc-ping.com/{os.environ.get("article_status_hc_ping")}', timeout=10)
