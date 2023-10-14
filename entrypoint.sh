65a1095664096fc93f56278d12b5e0b330f39d5a
Aaron Dominguez <aaron.dominguez@midokura.com>
2023-10-15 01:52:29 +0200

    Add celery worker to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@
 python3 manage.py migrate
 python3 manage.py wait_for_db
 python3 manage.py collectstatic
+celery -A celta_novas beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach
+celery -A celta_novas worker -l INFO -n default_worker --detach
 python3 manage.py runserver 0.0.0.0:8080
